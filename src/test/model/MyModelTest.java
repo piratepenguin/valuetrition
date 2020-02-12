57ae588b355548bd30d29de9672323da179aea54
deniskov <deniskov@live.com>
2020-02-12 08:50:49 -0800

    started tests

--- src/test/model/MyModelTest.java
+++ src/test/model/MyModelTest.java
@@ -1,7 +1,38 @@
 package model;
 
+import model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Test.*;
 
 class MyModelTest {
-    // delete or rename this class!
+
+    Food banana = new Food("banana", 0.7, 100, 24, 0, 1);
+    Food rice = new Food("rice", 5, 3000, 700, 11, 25;
+    Food buckwheat = new Food("buckwheat", 0.7, 100, 24, 0, 1);
+    Food apple = new Food("apple", 0.7, 100, 24, 0, 1);
+    Food milk = new Food("milk", 5, 1500, 250, 40, 15);
+    Food emptyFood = new Food("unnamed food", 0, 0, 0, 0, 0);
+
+    FoodList foodlist;
+
+    @BeforeEach
+    void beforeEach() {
+        foodlist = new FoodList();
+    }
+
+    @Test
+    void getterMethodTests() {
+        assertEquals(apple.getName(), "apple");
+        assertEquals(apple.getCalories(), "100");
+        assertEquals(apple.getCost(), 0.7);
+        assertEquals(apple.getCarbs(), 24);
+        assertEquals(apple.getFats(), 0);
+        assertEquals(apple.getProteins(), 1);
+        assertEquals(apple.getPrimaryType(), "carbs");
+    }
+
+
 }
