037a4bd21a2ebd450fed1f946d8339c2d1c1ccdd
deniskov7 <deniskov@live.com>
2020-03-16 22:52:11 -0700

    Made the stage have 2 scenes which can switch back and forth and have basic print functionality

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,11 +3,15 @@ package ui;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.util.Scanner;
 
@@ -15,33 +19,64 @@ import model.*;
 
 public class Main extends Application {
 
+    Stage window;
+    Scene scene1;
+    Scene scene2;
     Button button;
-
+    Button button2;
+    Button button3;
 
     public static void main(String[] args) {
-
         launch(args);
-        //NutritivityApp app = new NutritivityApp();
-
     }
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        primaryStage.setTitle("Nutritivity");
-        button = new Button();
-        button.setText("Let's Go!");
+        // setting window icon, title, border
+        window = primaryStage;
+        window.setTitle("Nutritivity");
+        window.getIcons().add(new Image(Main.class.getResourceAsStream("/NutritivityLogo.png")));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("/NutritivityLogo.png")));
+        // setting buttons for all future scenes
+        button = new Button("Let's Go!");
+        button2 = new Button("Go to window 2");
+        button3 = new Button("Go back to window 1");
 
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
+        button.setOnAction(e -> {
+            System.out.println("hello");
+            System.out.println("yo lambda is sick dude");
+        });
+        button2.setOnAction(e -> window.setScene(scene2));
+        button3.setOnAction(e -> window.setScene(scene1));
 
-        primaryStage.show();
+        // Scene 1 - uses vertical layout
+        VBox layout1 = new VBox(10);
+        Label label1 = new Label("Welcome Wizards!");
+        layout1.getChildren().addAll(label1, button, button2);
+        scene1 = new Scene(layout1, 300, 200);
 
+
+        // Scene 2 - uses horizontal layout
+        HBox layout2 = new HBox(15);
+        layout2.getChildren().add(button3);
+        scene2 = new Scene(layout2, 300, 250);
+
+
+        // initialize
+        window.setScene(scene1);
+        window.show();
     }
+
+
+//    @Override
+//    public void handle(ActionEvent event) {
+//        if (event.getSource() == button) {
+//            System.out.println("helo s");
+//         //NutritivityApp app = new NutritivityApp();
+//        }
+//    }
+
 }
 
